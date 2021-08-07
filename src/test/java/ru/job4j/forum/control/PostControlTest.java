91015c0fd0964ffac5d6588018d631f9eb9bec81
Александр <46612532+vovkalexander@users.noreply.github.com>
2021-08-07 04:09:49 +0300

    Spring Test исправил

--- src/test/java/ru/job4j/forum/control/PostControlTest.java
+++ src/test/java/ru/job4j/forum/control/PostControlTest.java
@@ -1,14 +1,21 @@
 package ru.job4j.forum.control;
-
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
-
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
+import static org.mockito.Mockito.verify;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -20,10 +27,14 @@ public class PostControlTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PostService posts;
+
     @Test
     @WithMockUser
     public void shouldReturnPostPage() throws Exception {
-        this.mockMvc.perform(get("/post?id=1"))
+        Mockito.when(posts.findById(8)).thenReturn(new Post());
+        this.mockMvc.perform(get("/post").param("id", "8"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("post"));
@@ -41,7 +52,8 @@ public class PostControlTest {
     @Test
     @WithMockUser
     public void shouldReturnEditPage() throws Exception {
-        this.mockMvc.perform(get("/edit?id=1"))
+        Mockito.when(posts.findById(8)).thenReturn(new Post());
+        this.mockMvc.perform(get("/edit").param("id", "8"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("edit"));
@@ -50,10 +62,10 @@ public class PostControlTest {
     @Test
     @WithMockUser
     public void shouldReturnCommentPage() throws Exception {
-        this.mockMvc.perform(get("/comment?id=1"))
+        Mockito.when(posts.findById(8)).thenReturn(new Post());
+        this.mockMvc.perform(get("/comment").param("id", "8"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("comment"));
     }
-
 }
